013fb48df84f6d727726f329ca1cc29c7315188e
George Castillo <gmcastil@gmail.com>
2024-02-22 12:04:55 -0700

    Removed LD_LIBRARY_PATH hacks since libfreetype can just be installed locally in /usr/local/lib

--- setup_modelsim.sh
+++ setup_modelsim.sh
@@ -12,31 +12,26 @@
 # environment or the location it points at is unavailable such as the NFS server
 # is down, this script is going to fail.
 if [[ "${BASH_SOURCE[0]}" -ef "${0}" ]]; then
-  printf '%s\n' >&2 "Must be sourced by or into a running bash shell"
-  exit 1
+    printf '%s\n' >&2 "Must be sourced by or into a running bash shell"
+    exit 1
 fi
 
 function err () {
-  printf 'Error: %s\n' >&2 "${1}"
-  return 0
+    printf 'Error: %s\n' >&2 "${1}"
+    return 0
 }
 
 TOOLS_ROOT=${TOOLS_ROOT:-'/tools'}
 
 modelsim_version='16.1'
-modelsim_root="${TOOLS_ROOT}/Questa/${modelsim_version}/modelsim_ase"
+modelsim_root="${TOOLS_ROOT}/Intel/ModelSim/${modelsim_version}/modelsim_ase"
 
 ok_to_export=0
 
-# Since the specific installation of ModelSim we're using has its own FreeType
-# library built we need to let the tool (ultimately the `vish` binary) know
-# about it - obviously, if it's not there, something is wrong
-if [[ ! -d "${modelsim_root}/lib32" ]]; then
-  err "No 32-bit libraries found at ${modelsim_root}/lib32"
-  ok_to_export=1
-else
-  LD_LIBRARY_PATH="${modelsim_root}/lib32:${LD_LIBRARY_PATH}"
-fi
+# In principle, any changes to LD_LIBRARY_PATH need to be made here prior to
+# exporting it later.  This is largely because a specific version of the 
+# 32-bit libfreetype library needs to be installed alongside the system version
+# and ModelSim will fail to run if it doesn't get found.
 
 # Also, even though the tools probably already set this, we set it ourselves
 # explicitly
@@ -45,15 +40,15 @@ MTI_VCO_MODE=32
 # Finally the path needs to be updated a bit so that the ModelSim binaries are
 # available
 if [[ ! -d "${modelsim_root}/bin" ]]; then
-  err "No ModelSim binaries directory at ${modelsim_root}/bin"
-  ok_to_export=1
+    err "No ModelSim binaries directory at ${modelsim_root}/bin"
+    ok_to_export=1
 else
-  PATH="${modelsim_root}/bin:${PATH}"
+    PATH="${modelsim_root}/bin:${PATH}"
 fi
 
 if [[ "${ok_to_export}" -ne 0 ]]; then
-  err "ModelSim directories not found. Check TOOLS_ROOT value."
-  exit 1
+    err "ModelSim directories not found. Check TOOLS_ROOT value."
+    return 1
 fi
 
 export LD_LIBRARY_PATH
@@ -61,5 +56,5 @@ export MTI_VCO_MODE
 export PATH
 
 command -v vsim >/dev/null 2>&1 || \
-  { err "vsim not found in PATH"; exit 1; }
+    { err "vsim not found in PATH"; return 1; }
 
