80bf577b7517a6550500e906f67be4bf24fcc4f1
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-03-04 05:14:51 -0700

    Fixed license

--- setup_questasim.sh
+++ setup_questasim.sh
@@ -8,7 +8,8 @@ questa_root="${TOOLS_ROOT}/Intel/FPGA/${questa_version}/questa_fe"
 MTI_VCO_MODE=64
 MODEL_TECH="${questa_root}/bin"
 PATH="${MODEL_TECH}:${PATH}"
-LM_LICENSE_FILE="${HOME}/licenses/LR-160810_License.dat"
+#LM_LICENSE_FILE="${HOME}/licenses/LR-160810_License.dat"
+LM_LICENSE_FILE="${HOME}/licenses/LR-222096_License.dat"
 
 export TOOLS_ROOT
 export MTI_VCO_MODE
